3d7163a451508cbccc9e1419436535cd3e90b0b0
dz00te <dz00te@framassa.org>
2026-02-01 18:39:07 +0100

    test homepage 1.0.0

--- build-images.sh
+++ build-images.sh
@@ -43,7 +43,7 @@ buildah config --entrypoint=/ \
     --label="org.nethserver.authorizations=traefik@node:routeadm" \
     --label="org.nethserver.tcp-ports-demand=1" \
     --label="org.nethserver.rootfull=0" \
-    --label="org.nethserver.images=ghcr.io/gethomepage/homepage:v0.10.9" \
+    --label="org.nethserver.images=ghcr.io/gethomepage/homepage:v1.0.0" \
     "${container}"
 # Commit the image
 buildah commit "${container}" "${repobase}/${reponame}"
